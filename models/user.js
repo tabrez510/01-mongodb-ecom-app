521121cee54c0c60a89c1909f57af32148c58edd
tabrez510 <alamtabrez510@gmail.com>
2024-03-16 20:25:02 +0530

    remove-items-from-cart

--- models/user.js
+++ models/user.js
@@ -21,6 +21,7 @@ const userSchema = new Schema({
    } 
 });
 
+
 userSchema.methods.addToCart = function(product){
     const cartProductIndex = this.cart.items.findIndex(cp => {
       return cp.productId.toString() === product._id.toString();
@@ -44,6 +45,14 @@ userSchema.methods.addToCart = function(product){
     this.cart = updatedCart
 
     return this.save();
+}
+
+userSchema.methods.removeFromCart = function(productId){
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
 }
 
 module.exports = mongoose.model('User', userSchema);
